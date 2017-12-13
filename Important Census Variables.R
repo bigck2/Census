9524bc92c543082e7a2a3e8ce0b73d0e65753e05
Chris Kiniry <chriskiniry@gmail.com>
2017-12-13 16:47:03 -0600

    data overload.

--- Important Census Variables.R
+++ Important Census Variables.R
@@ -95,6 +95,7 @@ my_vec <- c(housing_units = "B25001_001",
             tenure_owner_occupied = "B25003_002",
             tenure_renter_occupied = "B25003_003")
 
+
 my_vec <- c(tenure_by_edu_total = "B25013_001",
             tenure_by_edu_owner_total = "B25013_002",
             tenure_by_edu_owner_less_than_hs = "B25013_003",
@@ -116,6 +117,83 @@ my_vec <- c(median_gross_rent = "B25031_001",
             median_gross_rent_3_bed = "B25031_005",
             median_gross_rent_4_bed = "B25031_006",
             median_gross_rent_5_bed = "B25031_007")
+
+
+my_vec <- c(yr_built_total = "B25034_001",
+            yr_built_2014_or_later = "B25034_002",
+            yr_built_2010_to_2013 = "B25034_003",
+            yr_built_2000_to_2009 = "B25034_004",
+            yr_built_1990_to_1999 = "B25034_005",
+            yr_built_1980_to_1989 = "B25034_006",
+            yr_built_1970_to_1979 = "B25034_007",
+            yr_built_1960_to_1969 = "B25034_008",
+            yr_built_1950_to_1959 = "B25034_009",
+            yr_built_1940_to_1949 = "B25034_010",
+            yr_built_1939_or_earlier = "B25034_011",
+            yr_built_median_yr_built = "B25035_001"
+            )
+
+
+my_vec <- c(bdrm_total = "B25041_001",
+            bdrm_0_bed = "B25041_002",
+            bdrm_1_bed = "B25041_003",
+            bdrm_2_bed = "B25041_004",
+            bdrm_3_bed = "B25041_005",
+            bdrm_4_bed = "B25041_006",
+            bdrm_5__or_more_bed = "B25041_007"
+            )
+
+
+my_vec <- c(gr_to_income_total = "B25070_001",
+            gr_to_income_less_than_10 = "B25070_002",
+            gr_to_income_10_to_14_9 = "B25070_003",
+            gr_to_income_15_to_19_9 = "B25070_004",
+            gr_to_income_20_to_24_9 = "B25070_005",
+            gr_to_income_25_to_29_9 = "B25070_006",
+            gr_to_income_30_to_34_9 = "B25070_007",
+            gr_to_income_35_to_39_9 = "B25070_008",
+            gr_to_income_40_to_49_9 = "B25070_009",
+            gr_to_income_50_or_more = "B25070_010",
+            gr_to_income_not_computed = "B25070_011",
+            gr_to_income_median = "B25071_001"
+            )
+
+# Owner Occupied Housing Units Value
+my_vec <- c(value_total = "B25075_001",
+            value_less_than_10 = "B25075_002",
+            value_10_to_14_9 = "B25075_003",
+            value_15_to_19_9 = "B25075_004",
+            value_20_to_24_9 = "B25075_005",
+            value_25_to_29_9 = "B25075_006",
+            value_30_to_34_9 = "B25075_007",
+            value_35_to_39_9 = "B25075_008",
+            value_40_to_49_9 = "B25075_009",
+            value_50_to_59_9 = "B25075_010",
+            value_60_to_69_9 = "B25075_011",
+            value_70_to_79_9 = "B25075_012",
+            value_80_to_89_9 = "B25075_013",
+            value_90_to_99_9 = "B25075_014",
+            value_100_to_124_9 = "B25075_015",
+            value_125_to_149_9 = "B25075_016",
+            value_150_to_174_9 = "B25075_017",
+            value_175_to_199_9 = "B25075_018",
+            value_200_to_249_9 = "B25075_019",
+            value_250_to_299_9 = "B25075_020",
+            value_300_to_399_9 = "B25075_021",
+            value_400_to_499_9 = "B25075_022",
+            value_500_to_599_9 = "B25075_023",
+            value_750_to_999_9 = "B25075_024",
+            value_1m_to_1_49m = "B25075_025",
+            value_1_5m_to_1_9m = "B25075_026",
+            value_2m_or_more = "B25075_027"
+            )
+
+# Owner Occupied value
+my_vec <- c(value_lower_quartile = "B25076_001",
+            value_median_value = "B25077_001",
+            value_upper_quartile = "B25078_001")
+
+
 
 
 
